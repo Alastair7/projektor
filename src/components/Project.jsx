fc0c97e631cbd13ce204862fadc746d273222ede
Alastair7 <alexandru.ban.drmer@gmail.com>
2024-06-29 12:37:59 +0200

    PROJEKTOR | add dialog functionality for project details

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,25 +1,34 @@
 import "./Project.css";
-import { useState } from "react";
+import { useRef } from "react";
 import ProjectDetails from "./ProjectDetails";
-import { createPortal } from "react-dom";
 
 function Project({ data }) {
-  const [showModal, setShowModal] = useState(false);
+  const dialogRef = useRef(null);
+
+  const toggleDialog = () => {
+    if (!dialogRef.current) {
+      console.log("dialogRef is falsy");
+      return;
+    }
+    dialogRef.current.hasAttribute("open")
+      ? dialogRef.current.close()
+      : dialogRef.current.showModal();
+  };
 
   return (
     <>
-      <div onClick={() => setShowModal(true)} className="project">
+      <div onClick={toggleDialog} className="project">
         <h3>ID: {data.id}</h3>
         <h3>Title: {data.title}</h3>
         <h4>Description: {data.shortDescription}</h4>
         <h4>URL: {data.url}</h4>
         <h4>Status: {data.status}</h4>
       </div>
-      {showModal &&
-        createPortal(
-          <ProjectDetails project={data} onClose={() => setShowModal(false)} />,
-          document.body
-        )}
+      <ProjectDetails
+        project={data}
+        toggleDialog={toggleDialog}
+        ref={dialogRef}
+      />
     </>
   );
 }
--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -1,13 +1,16 @@
-function ProjectDetails({ project, onClose }) {
+import { forwardRef } from "react";
+
+const ProjectDetails = forwardRef(function ProjectDetails(props, ref) {
+  const { project, toggleDialog } = props;
   return (
-    <>
+    <dialog ref={ref}>
       <p>{project.title}</p>
       <p>{project.status}</p>
-      <button type="button" onClick={onClose}>
+      <button onClick={toggleDialog} type="button">
         Close
       </button>
-    </>
+    </dialog>
   );
-}
+});
 
 export default ProjectDetails;
